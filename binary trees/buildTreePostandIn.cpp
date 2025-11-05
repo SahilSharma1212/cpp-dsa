4c5b14922928285da10ca328c1f3d32195e6dcd4
Harry <sahilbhaisharma1212@gmail.com>
2025-11-05 16:45:48 +0530

    optimised approach

--- binary trees/buildTreePostandIn.cpp
+++ binary trees/buildTreePostandIn.cpp
@@ -3,50 +3,77 @@
 #include <algorithm>
 using namespace std;
 
-
-class Node {
+class Node
+{
 public:
     int value;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    Node(int val){
+    Node(int val)
+    {
         value = val;
         left = nullptr;
         right = nullptr;
     }
-
 };
 
-Node* buildTree(vector<int> preorder, vector<int> inorder){
-    if(preorder.empty()) return nullptr;
+Node *buildTree(vector<int> preorder, vector<int> inorder)
+{
+    if (preorder.empty())
+        return nullptr;
 
-    Node* newnode = new Node(preorder[0]);
-    
+    Node *newnode = new Node(preorder[0]);
+
     // find index of this value in inorder
     int idx = 0;
-    while(inorder[idx] != newnode->value) idx++;
+    while (inorder[idx] != newnode->value)
+        idx++;
 
     // new arrays for left
-    vector<int> newpreL(preorder.begin() + 1 ,preorder.begin() + idx + 1);
-    vector<int> newinL(inorder.begin() , inorder.begin() + idx);
+    vector<int> newpreL(preorder.begin() + 1, preorder.begin() + idx + 1);
+    vector<int> newinL(inorder.begin(), inorder.begin() + idx);
 
     // new array for the right
     vector<int> newpreR(preorder.begin() + idx + 1, preorder.end());
-    vector<int> newinR(inorder.begin()+idx+1,inorder.end());
+    vector<int> newinR(inorder.begin() + idx + 1, inorder.end());
 
-
-    newnode->left = buildTree(newpreL,newinL);
-    newnode->right = buildTree(newpreR,newinR);
+    newnode->left = buildTree(newpreL, newinL);
+    newnode->right = buildTree(newpreR, newinR);
 
     return newnode;
-
 }
 
+Node *buildTreeHelper(const vector<int> &preorder, int preStart, int preEnd,
+                      const vector<int> &inorder, int inStart, int inEnd)
+{
+    if (preStart > preEnd || inStart > inEnd)
+        return nullptr;
 
-void preOrderTraversal(Node* root){
+    int rootVal = preorder[preStart];
+    Node *root = new Node(rootVal);
 
-    if(root == nullptr) return;
+    // find root index in inorder
+    int idx = inStart;
+    while (inorder[idx] != rootVal)
+        idx++;
+
+    int leftSize = idx - inStart;
+
+    root->left = buildTreeHelper(preorder, preStart + 1, preStart + leftSize,
+                                 inorder, inStart, idx - 1);
+
+    root->right = buildTreeHelper(preorder, preStart + leftSize + 1, preEnd,
+                                  inorder, idx + 1, inEnd);
+
+    return root;
+}
+
+void preOrderTraversal(Node *root)
+{
+
+    if (root == nullptr)
+        return;
 
     cout << root->value << "  ";
 
@@ -54,24 +81,26 @@ void preOrderTraversal(Node* root){
     preOrderTraversal(root->right);
 }
 
-void inOrderTraversal(Node* root){
+void inOrderTraversal(Node *root)
+{
 
-    if(root == nullptr) return;
+    if (root == nullptr)
+        return;
 
     inOrderTraversal(root->left);
-    
+
     cout << root->value << "  ";
 
     inOrderTraversal(root->right);
 }
 
+int main()
+{
 
-int main() {
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
 
-    vector<int> preorder = {3,9,20,15,7};
-    vector<int> inorder = {9,3,15,20,7};
-    
-    Node* root  = buildTree(preorder,inorder);
+    Node *root = buildTree(preorder, inorder);
 
     preOrderTraversal(root);
     cout << endl;
